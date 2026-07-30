f996512b2c864dcdd4c9731627992e27b84ca7a9
muchu999 <christi999@hotmail.com>
2026-07-30 12:57:55 -0400

    cleanup

--- xbmc/cores/VideoPlayer/VideoRenderers/windows/RendererBase.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/windows/RendererBase.h
@@ -212,8 +212,6 @@ protected:
 
   AVPixelFormat m_format = AV_PIX_FMT_NONE;
   CD3DTexture m_IntermediateTarget;
-  CD3DTexture m_TempTarget;
-  Microsoft::WRL::ComPtr < ID3D11VideoProcessorOutputView> m_pTempTargetView;
 
   std::shared_ptr<COutputShader> m_outputShader;
   std::unique_ptr<CColorManager> m_colorManager;
